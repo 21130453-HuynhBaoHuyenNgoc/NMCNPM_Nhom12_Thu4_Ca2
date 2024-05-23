6c1bccc3fa215680d97f8c4feceb8c909a89f20b
21130453-HuynhBaoHuyenNgoc <21130453@st.hcmuaf.edu.vn>
2024-05-23 20:09:26 +0700

    add select date and spinner

--- app/src/main/java/com/example/myapplication/CreateNoteFragment.java
+++ app/src/main/java/com/example/myapplication/CreateNoteFragment.java
@@ -1,12 +1,30 @@
 package com.example.myapplication;
 
+import static java.security.AccessController.getContext;
+
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,12 +32,58 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CreateNoteFragment extends Fragment {
-
+    private EditText etAmount, etDescription;
+    private Button btnSelectDate, btnAddTransaction, tvSelectCategory;
+    private TextView tvDate;
+    private ImageButton btnBack;
+    private Spinner spin;
+    String[] items = { "Thu tiền", "Chi tiền" };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_note, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_note, container, false);
+        // Kết nối các thành phần UI
+        etAmount = view.findViewById(R.id.etAmount);
+        etDescription = view.findViewById(R.id.etDescription);
+        btnSelectDate = view.findViewById(R.id.btnSelectDate);
+        btnAddTransaction = view.findViewById(R.id.btnAddTransaction);
+        tvSelectCategory = view.findViewById(R.id.tvSelectCategory);
+        tvDate = view.findViewById(R.id.tvDate);
+        btnBack = view.findViewById(R.id.btn_back);
+        spin = view.findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), R.layout.custom_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Thiết lập ArrayAdapter cho Spinner
+        spin.setAdapter(adapter);
+
+        // Đặt sự kiện cho các nút bấm
+        btnBack.setOnClickListener(v -> getActivity().onBackPressed());
+        btnSelectDate.setOnClickListener(v -> showDatePickerDialog());
+        btnAddTransaction.setOnClickListener(v -> createNewNote());
+//        tvSelectCategory.setOnClickListener(v -> showCategorySelectionDialog());
+
+        // Đặt ngày hiện tại cho tvDate
+        tvDate.setText(new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date()));
+
+        return view;
     }
-}
+    private void showDatePickerDialog() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                (view, year1, monthOfYear, dayOfMonth) -> tvDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1),
+                year, month, day);
+        datePickerDialog.show();
+    }
+
+    private void createNewNote() {
+
+    }
+    }
---
+++ app/src/main/java/com/example/myapplication/model/Note.java
@@ -0,0 +1,67 @@
+package com.example.myapplication.model;
+
+public class Note {
+    private String id;
+    private String idUser;
+    private double amount;
+    private double availableAmount;
+    private String type;
+    private String date;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAvailableAmount() {
+        return availableAmount;
+    }
+
+    public void setAvailableAmount(double availableAmount) {
+        this.availableAmount = availableAmount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
