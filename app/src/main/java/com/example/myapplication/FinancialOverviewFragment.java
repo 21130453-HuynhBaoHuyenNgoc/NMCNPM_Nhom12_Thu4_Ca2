74d91b0adc0420a7a5a95e0778dee8793214d360
nttduongg <21130032@st.hcmuaf.edu.vn>
2024-05-27 05:43:24 +0700

    bat ngoai le chon thoi gian khong hop le

--- app/src/main/java/com/example/myapplication/FinancialOverviewFragment.java
+++ app/src/main/java/com/example/myapplication/FinancialOverviewFragment.java
@@ -8,41 +8,54 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.myapplication.dao.DatabaseHelper;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 //tao fragment Barchart
 public class FinancialOverviewFragment extends Fragment {
     //khai bao ra cac edittext va cac button tuong ung voi fragment_financial_overview
     EditText edit_startDate, edit_endDate, edit_tienthu, edit_tienchi;
     Button btt_StartDate, btt_endDate;
-
+    DatabaseHelper databaseHelper;
+    SimpleDateFormat simpleDateFormat;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_financial_overview, container, false);
+       //tham chieu den cac budget trong layout
         edit_startDate = view.findViewById(R.id.edit_rep_start);
         edit_endDate = view.findViewById(R.id.edit_rep_end);
         btt_StartDate = view.findViewById(R.id.btt_rep_start);
         btt_endDate = view.findViewById(R.id.btt_rep_end);
 
-        //chon thoi gian bat dau
+        databaseHelper = new DatabaseHelper(getContext());
+        simpleDateFormat = new SimpleDateFormat("dd/mm/yyyy", Locale.getDefault());
+
+        //1. Nguoi dung chon thoi gian bat dau
         btt_StartDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //hien thi thoi gian bat dau khi nguoi dung chon
+                //2. He thong hien thi datepicker cho nguoi dung chon thoi gian bat dau
                 showDatepicker(edit_startDate);
             }
         });
 
-        //chon thoi gian ket thuc
+        //3. Nguoi dung chon thoi gian ket thuc
         btt_endDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //4.He thong hien thi datepicker cho nguoi dung chon thoi gian ket thuc
                 showDatepicker(edit_endDate);
+                calculateIncome();
+
             }
         });
 
@@ -60,6 +73,7 @@ public class FinancialOverviewFragment extends Fragment {
                 new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        //He thong cap nhat ngay
                         date.setText(dayOfMonth + "/" + (month+1) + "/" + year);
                     }
                 }, year, month, day);
@@ -67,5 +81,31 @@ public class FinancialOverviewFragment extends Fragment {
         datePickerDialog.show();
     }
 //rang buoc ngay bat dau phai truoc ngay ket thuc
+
+    //phuong thuc tinh tong tien thu
+    public void calculateIncome(){
+        String startDate = edit_startDate.getText().toString();
+        String endDate = edit_endDate.getText().toString();
+        //neu thoi gian bat dau va ket thuc chua duoc chon
+        if(startDate.isEmpty() || endDate.isEmpty()){
+            //hien thi thong bao Vui long chon ngay
+            Toast.makeText(getContext(), "Vui lòng chọn ngày", Toast.LENGTH_LONG).show();
+            return;
+        }
+        try{
+            Long st_Date = simpleDateFormat.parse(startDate).getTime();
+            Long e_Date = simpleDateFormat.parse(endDate).getTime();
+
+            //rang buoc ngay bat dau truoc ngay ket thuc
+            if(st_Date > e_Date){
+                //hien thi thong bao Thời gian không hợp lệ
+                Toast.makeText(getContext(), "Thời gian không hợp lệ", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+        }catch (Exception e){
+
+        }
+    }
 
 }
