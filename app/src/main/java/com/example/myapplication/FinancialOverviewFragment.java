9306fca6bbc1190a1e2cb6966e3c26a87136d323
nttduongg <21130032@st.hcmuaf.edu.vn>
2024-05-29 11:32:45 +0700

    them comment giai thich theo tung buoc sequence

--- app/src/main/java/com/example/myapplication/FinancialOverviewFragment.java
+++ app/src/main/java/com/example/myapplication/FinancialOverviewFragment.java
@@ -45,20 +45,20 @@ public class FinancialOverviewFragment extends Fragment {
         databaseHelper = new DatabaseHelper(getContext());
         simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
 
-        // 1. Nguoi dung chon thoi gian bat dau
+        // 6.1. Người dùng nhấp vào button Từ ngày
         btt_StartDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 2. He thong hien thi datepicker cho nguoi dung chon thoi gian bat dau
+                // 6.5.Hệ thống hiển thị ngày được chọn ở editText Ngày bắt đầu
                 showDatepicker(edit_startDate);
             }
         });
 
-        // 3. Nguoi dung chon thoi gian ket thuc
+        // 6.6. Người dùng nhấp vào button Đến ngày
         btt_endDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 4. He thong hien thi datepicker cho nguoi dung chon thoi gian ket thuc
+                // 6.10. Hệ thống hiển thị ngày được chọn ở editText Ngày kết thúc
                 showDatepicker(edit_endDate);
             }
         });
@@ -67,6 +67,7 @@ public class FinancialOverviewFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
+                    //6.16. Hệ thống hiển thị số tiền ở Mức thu và Mức chi
                     updateFinancialOverview();
                 }
             }
@@ -78,40 +79,48 @@ public class FinancialOverviewFragment extends Fragment {
         return view;
     }
 
+    // Phương thức hiển thị số tiền ở Mức thu và Mức chi
     private void updateFinancialOverview() {
         calculateTotalIncome();
         calculateTotalExpense();
     }
 
-    // phuong thuc hien thi datepickerdialog
+    // 6.2. và 6.7. Hệ thống hiển thị datepicker dialog
     private void showDatepicker(final EditText date) {
+        //lấy ngày thang nam hien tai
         final Calendar c = Calendar.getInstance();
         int day = c.get(Calendar.DAY_OF_MONTH);
         int month = c.get(Calendar.MONTH);
         int year = c.get(Calendar.YEAR);
-
+        //6.2. và 6.7 Mặc định là ngày tháng năm hiện tại
         DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                //6.3.và 6.8. Người dùng chọn ngày
+                //6.4.và 6.9. Người dùng nhấn OK
                 new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        // He thong cap nhat ngay
+                        //editText duoc cap nhat ngay thang nam tu thoi gian nguoi dung chon
                         date.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
                         updateFinancialOverview();
                     }
                 }, year, month, day);
-        // hien thi datepicker dialog
         datePickerDialog.show();
     }
 
-    // phuong thuc tinh tong tien thu
+    //phuong thuc tinh tong chi
     private void calculateTotalIncome() {
         String startDate = edit_startDate.getText().toString();
         String endDate = edit_endDate.getText().toString();
 
-        // neu thoi gian bat dau va ket thuc chua duoc chon
-        if (startDate.isEmpty() || endDate.isEmpty()) {
+        // Alternative flow <Chưa chọn thời gian bắt đầu>
+        if (startDate.isEmpty()) {
             // hien thi thong bao Vui long chon ngay
-            Toast.makeText(getContext(), "Vui lòng chọn ngày", Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "Vui lòng chọn ngày bắt đầu", Toast.LENGTH_LONG).show();
+            return;
+        }
+        //Alternative flow <Chưa chọn thời gian kết thúc>
+        if(endDate.isEmpty()){
+            Toast.makeText(getContext(), "Vui lòng chọn ngày kết thúc", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -119,17 +128,16 @@ public class FinancialOverviewFragment extends Fragment {
             long st_Date = simpleDateFormat.parse(startDate).getTime();
             long e_Date = simpleDateFormat.parse(endDate).getTime();
 
-            // rang buoc ngay bat dau truoc ngay ket thuc
+            // 6.11. Hệ thống kiểm tra thời gian bắt đầu có trước thời gian kết thúc không
             if (st_Date > e_Date) {
-                // hien thi thong bao Thời gian không hợp lệ
+                // Alternative flow <Thời gian không hợp lệ>
                 Toast.makeText(getContext(), "Thời gian không hợp lệ", Toast.LENGTH_LONG).show();
                 return;
             }
 
             // thuc hien truy van
             SQLiteDatabase db = databaseHelper.getReadableDatabase();
-
-            // truy van tong thu
+            // 6.12.  Hệ thống lấy từ cơ sở dữ liệu tổng số tiền thu ở thời gian người dùng chọn
             String sqlThu = "SELECT SUM(" + DatabaseHelper.COLUMN_AMOUNT + ") FROM " + DatabaseHelper.TABLE_CREATE_NOTE +
                     " WHERE " + DatabaseHelper.COLUMN_TYPE + " = ? AND " + DatabaseHelper.COLUMN_DATE + " BETWEEN ? AND ?";
             // Dung Cursor
@@ -144,21 +152,28 @@ public class FinancialOverviewFragment extends Fragment {
             cursorThu.close();
 
             // setText cho edit_thu
+            //6.13. Hệ thống cập nhật số tiền cho Mức thu
             edit_tienthu.setText(addCommas(String.valueOf(totalThu)));
 
         } catch (Exception e) {
             Toast.makeText(getContext(), "Định dạng ngày không hợp lệ", Toast.LENGTH_LONG).show();
         }
     }
-    // phuong thuc tinh tong tien chi
+
+    //phuong thuc tinh tong chi
     private void calculateTotalExpense() {
         String startDate = edit_startDate.getText().toString();
         String endDate = edit_endDate.getText().toString();
 
-        // neu thoi gian bat dau va ket thuc chua duoc chon
-        if (startDate.isEmpty() || endDate.isEmpty()) {
+        // Alternative flow <Chưa chọn thời gian bắt đầu>
+        if (startDate.isEmpty()) {
             // hien thi thong bao Vui long chon ngay
-            Toast.makeText(getContext(), "Vui lòng chọn ngày", Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "Vui lòng chọn ngày bắt đầu", Toast.LENGTH_LONG).show();
+            return;
+        }
+        // Alternative flow <Chưa chọn thời gian kết thúc>
+        if(endDate.isEmpty()){
+            Toast.makeText(getContext(), "Vui lòng chọn ngày kết thúc", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -166,9 +181,9 @@ public class FinancialOverviewFragment extends Fragment {
             long st_Date = simpleDateFormat.parse(startDate).getTime();
             long e_Date = simpleDateFormat.parse(endDate).getTime();
 
-            // rang buoc ngay bat dau truoc ngay ket thuc
+            // 6.11. Hệ thống kiểm tra thời gian bắt đầu có trước thời gian kết thúc không
             if (st_Date > e_Date) {
-                // hien thi thong bao Thời gian không hợp lệ
+                // Alternative flow <Thời gian không hợp lệ>
                 Toast.makeText(getContext(), "Thời gian không hợp lệ", Toast.LENGTH_LONG).show();
                 return;
             }
@@ -176,7 +191,7 @@ public class FinancialOverviewFragment extends Fragment {
             // thuc hien truy van
             SQLiteDatabase db = databaseHelper.getReadableDatabase();
 
-            // truy van tong thu
+            // 6.14. Hệ thống lấy từ cơ sở dữ liệu tổng số tiền chi ở thời gian người dùng chọn
             String sqlChi = "SELECT SUM(" + DatabaseHelper.COLUMN_AMOUNT + ") FROM " + DatabaseHelper.TABLE_CREATE_NOTE +
                     " WHERE " + DatabaseHelper.COLUMN_TYPE + " = ? AND " + DatabaseHelper.COLUMN_DATE + " BETWEEN ? AND ?";
             // Dung Cursor
@@ -191,6 +206,7 @@ public class FinancialOverviewFragment extends Fragment {
             cursorChi.close();
 
             // setText cho edit_thu
+            // 6.15. Hệ thống cập nhật số tiền cho Mức chi
             edit_tienchi.setText(addCommas(String.valueOf(totalChi)));
 
         } catch (Exception e) {
